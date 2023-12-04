bfdcc35ce4606046ce775de28c6ecdb924980ee3
Lezzycan <waheedolalekan23@gmail.com>
2023-12-04 02:57:21 +0100

    colors

---
+++ lib/utils/constants/colors.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class LColors {
+  LColors._();
+
+  // App BASIC COLORS
+  static const Color primaryColor = Color(0XFF4B68FF);
+  static const Color secondaryColor = Color(0XFFFFE248);
+  static const Color accent = Color(0XFFB0C7FF);
+
+  // Text Colors
+  static const Color textPrimary = Color(0XFF333333);
+  static const Color textSecondary = Color(0XFF6C757D);
+  static const Color textWhite = Colors.white;
+
+  /// Gradient Colors
+   
+
+  // Background Colors
+  static const Color light = Color(0XFFF6F6F6);
+  static const Color dark = Color(0XFF272727);
+  static const Color primaryBackground = Color(0XFFF3F5FF);
+
+  // Background Container Colors
+  static const Color lightContainer = Color(0XFFF6F6F6);
+  static  Color darkContainer = Colors.white.withOpacity(0.1);
+
+ // Button Colors
+  static const Color buttonPrimary = Color(0XFF4B68FF); 
+  static const Color buttonSecondary = Color(0XFF6C757D);
+  static const Color buttonDisabled = Color(0XFFC4C4C4);
+
+  // Border Colors
+  static const Color borderPrimary = Color(0XFFD9D9D9);
+  static const Color borderSecondary = Color(0XFFE6E6E6);
+
+  // Error and Validatoon Colors
+  static const Color error = Color(0XFFD32F2F);
+  static const Color success = Color(0XFF388E3C);
+  static const Color warning = Color(0XFFF57C00);
+  static const Color info = Color(0XFF1976D2);
+
+
+  /// Neutral Shades
+  
+  static const Color black = Color(0XFF232323);
+  static const Color darkerGrey = Color(0XFF4F4F4F);
+  static const Color darkGrey = Color(0XFF939393);
+  static const Color grey = Color(0XFFE0E0E0);
+}
+
+
